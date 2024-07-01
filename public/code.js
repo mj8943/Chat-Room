380f44df9a190f90de9767a02e9013051466df1a
Minhaj K <minhajmazah@gmail.com>
2024-07-01 20:56:48 +0530

    Polling feature added

--- public/code.js
+++ public/code.js
@@ -21,7 +21,6 @@
         let file = fileInput.files[0];
 
         if (file) {
-
             let reader = new FileReader();
             reader.onload = function(e) {
                 socket.emit("chat", {
@@ -64,6 +63,45 @@
         renderMessage("other", message);
     });
 
+    // Poll creation and voting
+    const pollButton = app.querySelector(".chat-screen #poll-button");
+    const pollModal = document.getElementById("poll-modal");
+    const closeModal = document.querySelector(".close");
+
+    pollButton.addEventListener("click", function() {
+        pollModal.style.display = "block";
+    });
+
+    closeModal.addEventListener("click", function() {
+        pollModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == pollModal) {
+            pollModal.style.display = "none";
+        }
+    });
+
+    document.getElementById("create-poll").addEventListener("click", function() {
+        const question = document.getElementById("poll-question").value;
+        const options = Array.from(document.querySelectorAll(".poll-option")).map(input => input.value);
+        if (question.length === 0 || options.some(opt => opt.length === 0)) {
+            return;
+        }
+        socket.emit("createPoll", { username: uname, question, options });
+        pollModal.style.display = "none";
+        document.getElementById("poll-question").value = "";
+        document.querySelectorAll(".poll-option").forEach(input => input.value = "");
+    });
+
+    socket.on("newPoll", (poll) => {
+        renderPoll(poll);
+    });
+
+    socket.on("updateResults", (voteCounts) => {
+        updatePollResults(voteCounts);
+    });
+
     function renderMessage(type, message) {
         let messageContainer = app.querySelector(".chat-screen .messages");
         let el = document.createElement("div");
@@ -98,5 +136,81 @@
 
         messageContainer.appendChild(el);
         messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+    }
+
+    function renderPoll(poll) {
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        let el = document.createElement("div");
+        el.setAttribute("class", "poll-message");
+
+        let pollHtml = `
+            <div>
+                <div class="name">${poll.username}</div>
+                <div class="question">${poll.question}</div>
+                <div class="options">`;
+
+        poll.options.forEach(option => {
+            pollHtml += `<button class="poll-option" data-pollId="${poll.id}" data-option="${option}">${option} (<span class="vote-count">0</span>)</button>`;
+        });
+
+        pollHtml += `</div></div>`;
+
+        el.innerHTML = pollHtml;
+        messageContainer.appendChild(el);
+
+        const optionButtons = el.querySelectorAll(".poll-option");
+        optionButtons.forEach(button => {
+            button.addEventListener("click", function() {
+                const pollId = button.getAttribute("data-pollId");
+                const option = button.getAttribute("data-option");
+                socket.emit("vote", {
+                    pollId: pollId,
+                    option: option,
+                    username: uname // Send username with vote
+                });
+
+                // Disable other options
+                optionButtons.forEach(optButton => {
+                    if (optButton !== button) {
+                        optButton.disabled = true;
+                    }
+                });
+
+                button.disabled = true; // Disable selected option after voting
+            });
+        });
+
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+    }
+
+    function updatePollResults(voteCounts) {
+        const pollMessages = document.querySelectorAll(".poll-message");
+        pollMessages.forEach(pollMessage => {
+            const optionButtons = pollMessage.querySelectorAll(".poll-option");
+            let maxCount = 0;
+            let maxOption = null;
+
+            optionButtons.forEach(button => {
+                const option = button.getAttribute("data-option");
+                const countSpan = button.querySelector(".vote-count");
+                const count = voteCounts[option] || 0;
+                countSpan.innerText = count;
+
+                // Determine the option with the maximum votes
+                if (count > maxCount) {
+                    maxCount = count;
+                    maxOption = button;
+                }
+            });
+
+            // Highlight the option with the maximum votes
+            optionButtons.forEach(button => {
+                if (button === maxOption) {
+                    button.classList.add("max-vote-option");
+                } else {
+                    button.classList.remove("max-vote-option");
+                }
+            });
+        });
     }
 })();
--- server.js
+++ server.js
@@ -7,15 +7,41 @@ const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+let polls = [];
+let pollVotes = {};
+
 io.on("connection", function(socket) {
     socket.on("newuser", function(username) {
         socket.broadcast.emit("update", username + " joined the conversation");
     });
+
     socket.on("exituser", function(username) {
         socket.broadcast.emit("update", username + " left the conversation");
     });
+
     socket.on("chat", function(message) {
         socket.broadcast.emit("chat", message);
+    });
+
+    socket.on("createPoll", function(poll) {
+        poll.id = polls.length + 1;
+        polls.push(poll);
+        pollVotes[poll.id] = {};
+        poll.options.forEach(option => {
+            pollVotes[poll.id][option] = 0;
+        });
+        io.emit("newPoll", poll);
+    });
+
+    socket.on("vote", function(vote) {
+        const { pollId, option, username } = vote;
+
+        // Check if user has already voted for this poll
+        if (pollVotes[pollId] && !pollVotes[pollId][username]) {
+            pollVotes[pollId][username] = true;
+            pollVotes[pollId][option] += 1;
+            io.emit("updateResults", pollVotes[pollId]);
+        }
     });
 });
 
